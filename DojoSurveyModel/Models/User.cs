47314b00a8b03d4d98cbf09ac0d2078b84d41ff6
CameronParke <campaign711@gmail.com>
2022-04-11 07:03:06 -0500

    Dojo Survey with Model

---
+++ DojoSurveyModel/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DojoSurveyModel.Models
+{
+    public class User
+    {
+        public string name {get;set;}
+        public string comment {get;set;}
+        public string location {get;set;}
+        public string language {get;set;}
+    }
+}
